b30d14b497a18960853cf5635c3004936d6d11a5
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-16 22:11:22 -0400

    add user to the database configuration

--- config.php
+++ config.php
@@ -3,9 +3,10 @@
   return[
     'database' => [
       'host' => 'localhost',
+      'user' => 'root',
       'port' => '3306',
       'dbname' => 'coffee_db',
-      'charset' => 'utf9mb4',
+      'charset' => 'utf8mb4',
       'password' => 'anaphoric-panchions-sdeign'
     ]
   ]
