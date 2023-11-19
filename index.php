a4e73ee25dc36d1c94e861861546c08b91c27c41
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-19 18:37:47 -0400

    implement header and footer and style homepage

--- index.php
+++ index.php
@@ -9,20 +9,7 @@
   
     // dd($users);
     // dd($_SERVER);
+    require './views/home.php';
+
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="./css/styles.css">
-</head>
-<body>
-
-  <?php require './views/home.php' ?>
-  
-</body>
-</html>
