b64cc40f85097e44ebbbd21528fffa773818ceeb
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-24 23:51:35 -0400

    create a file to design the layout for the 404 page

---
+++ views/404.php
@@ -0,0 +1,11 @@
+<?php require view('partials/head.php'); ?>
+
+<?php require view('partials/nav.php'); ?>
+
+<?php require view('partials/banner.php'); ?>
+
+<main>
+  <p>This is a 404 page!</p>
+</main>
+
+<?php require view('partials/footer.php'); ?>
