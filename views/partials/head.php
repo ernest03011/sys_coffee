276dc6bcaf6fc0ccc66e0c596e07c73a4344698e
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-24 23:50:23 -0400

    create a partial folder with each part of the website, head, nav, banner, header, and footer

---
+++ views/partials/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-white">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/styles.css">
+  <title>A Coffee Place</title>
+</head>
+<body class="h-full">
+
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
