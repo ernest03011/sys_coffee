75cd7bb338c1412a4a9f3adad842541bb8f057f4
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-25 00:04:17 -0400

    Remove intentionally deleted files from tracking

--- views/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-primary-brown text-black p-6">
-    <div class="container mx-auto text-center">
-        <p class="text-sm">&copy; 2023 Coffee Recipe Repository. All rights reserved.</p>
-        <p class="text-sm">Designed with ☕️ and <i class="fab fa-heart text-red-500"></i> by Manuel De Jesus</p>
-    </div>
-</footer>
