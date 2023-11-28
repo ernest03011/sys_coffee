94fe184376442580b217153c3df005da0f79c38b
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-28 09:14:22 -0400

    update the routes by removing the two dots at the beggining of the path

--- routes.php
+++ routes.php
@@ -2,12 +2,13 @@
 
 $router = new Router();
 
-$router->get('/', '../controller/index.php');
-$router->get('/recipes', '../controller/recipes.php');
-$router->get('/about', '../controller/about.php');
-$router->get('/login', '../controller/login.php');
-$router->get('/register', '../controller/register.php');
-$router->get('/submit-recipe', '../controller/submit_recipe.php');
-$router->get('/recipe', '../controller/recipe.php');
+$router->get('/', '/controller/index.php');
+$router->get('/recipes', '/controller/recipes.php');
+$router->get('/about', '/controller/about.php');
+$router->get('/login', '/controller/login.php')->only('guest');
+$router->get('/register', '/controller/register.php');
+$router->get('/submit-recipe', '/controller/submit_recipe.php');
+$router->get('/recipe', '/controller/recipe.php');
+// $router->get('/test', '../controller/testing.php');
 
 
