f01db3819934371e3edd968a8651db5e52eaeb39
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-11-28 09:28:41 -0400

    update the registration routes

--- routes.php
+++ routes.php
@@ -6,7 +6,10 @@ $router->get('/', '/controller/index.php');
 $router->get('/recipes', '/controller/recipes.php');
 $router->get('/about', '/controller/about.php');
 $router->get('/login', '/controller/login.php')->only('guest');
-$router->get('/register', '/controller/register.php');
+
+$router->get('/register', '/controller/registration/create.php')->only('guest');
+$router->post('/register', '/controller/registration/store.php')->only('guest');
+
 $router->get('/submit-recipe', '/controller/submit_recipe.php');
 $router->get('/recipe', '/controller/recipe.php');
 // $router->get('/test', '../controller/testing.php');
