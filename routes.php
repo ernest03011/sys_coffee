39ff0a4c13297c09280671311d0e955999eeb1c6
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-06 18:00:46 -0400

    chore: update(routes): modify routes in the routes.php file

--- routes.php
+++ routes.php
@@ -5,13 +5,16 @@ $router = new Router();
 $router->get('/', '/controller/index.php');
 $router->get('/recipes', '/controller/recipes.php');
 $router->get('/about', '/controller/about.php');
-$router->get('/login', '/controller/login.php')->only('guest');
+
+$router->get('/login', '/controller/session/create.php')->only('guest');
+$router->post('/login', '/controller/session/store.php')->only('guest');
+$router->post('/logout', '/controller/session/destroy.php')->only('auth');
 
 $router->get('/register', '/controller/registration/create.php')->only('guest');
 $router->post('/register', '/controller/registration/store.php')->only('guest');
 
 $router->get('/submit-recipe', '/controller/submit_recipe.php');
 $router->get('/recipe', '/controller/recipe.php');
-// $router->get('/test', '../controller/testing.php');
+
 
 
