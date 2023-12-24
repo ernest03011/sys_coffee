3ebe41a74f813a50c1a58fb2c65bf6072ab345e2
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-24 12:56:21 -0400

    chore: remove(comments): eliminate unnecessary comments from the codebase

--- public/index.php
+++ public/index.php
@@ -9,10 +9,6 @@ $router = require base_path('Router.php');
 require base_path('routes.php');
 
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-// a hidden input should be added to the post 
-// with the name _method and value DELETE or PATCH
-// FORM can only use put and get request
  
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
