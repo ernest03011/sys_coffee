5a49e85f18520cfc423e50eb853428cf08143978
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-24 11:56:21 -0400

    fix: check(class-existence): update code to confirm if a class already exists or not

--- controller/index.php
+++ controller/index.php
@@ -1,3 +1,13 @@
 <?php
 
+if (!class_exists('Database')) {
+  // If not, require it
+  require base_path('Database.php');
+}
+$config = require base_path('config.php');
+
+$db = new Database($config['database']);
+$recipes = $db->query("select * from recipes LIMIT 3")->get();
+
+
 require view('home.php');
