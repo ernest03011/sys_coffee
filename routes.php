90043398a1e18838ee6b39ce9b6c2a29cb368ba9
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-24 12:15:15 -0400

    chore: add(routes): incorporate additional routes into the project

--- routes.php
+++ routes.php
@@ -3,8 +3,12 @@
 $router = new Router();
 
 $router->get('/', '/controller/index.php');
-$router->get('/recipes', '/controller/recipes.php');
 $router->get('/about', '/controller/about.php');
+$router->get('/test', '/controller/test.php');
+
+$router->get('/contact', '/controller/contact.php');
+$router->post('/contact', '/controller/email/send.php');
+
 
 $router->get('/login', '/controller/session/create.php')->only('guest');
 $router->post('/login', '/controller/session/store.php')->only('guest');
@@ -13,8 +17,18 @@ $router->post('/logout', '/controller/session/destroy.php')->only('auth');
 $router->get('/register', '/controller/registration/create.php')->only('guest');
 $router->post('/register', '/controller/registration/store.php')->only('guest');
 
-$router->get('/submit-recipe', '/controller/submit_recipe.php');
-$router->get('/recipe', '/controller/recipe.php');
+$router->get('/submit-recipe', '/controller/recipes/create.php')->only('auth');
+$router->post('/submit-recipe', '/controller/recipes/store.php')->only('auth');
+
+$router->get('/recipes', '/controller/recipes/index.php')->only('auth');
+$router->get('/recipe', '/controller/recipes/show.php')->only('auth');
+$router->delete('/recipe', '/controller/recipes/destroy.php')->only('auth');
 
 
+$router->get('/recipe/edit/', '/controller/recipes/edit.php')->only('auth');
+$router->patch('/recipe', '/controller/recipes/update.php')->only('auth');
 
+$router->post('/rating', '/controller/rating/create.php')->only('auth');
+
+$router->get('/membership', '/controller/memberships/index.php')->only('auth');
+$router->post('/membership', '/controller/memberships/store.php')->only('auth');
