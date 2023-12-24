4f1c2aa5242b05fe5afec1b9d2b3202e727c7f90
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-24 12:51:23 -0400

    chore: update(nav-menu): expand nav menu by adding more items, implement logic to display options based on user authentication status

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -6,18 +6,26 @@
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
         </a>
       </div>
+      <!-- Hamburguer icon - OPEN BUTTON -->
       <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+        <button type="button" id="openNavModal" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Open main menu</span>
           <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
       </div>
+
       <div class="hidden lg:flex lg:gap-x-12">
         <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
-        <a href="/recipes" class="text-sm font-semibold leading-6 text-gray-900">Recipes</a>
-        <a href="/about" class="text-sm font-semibold leading-6 text-gray-900">About us</a>
+        <?php if(isset($_SESSION['user']['email'])) : ?>        
+          <a href="/recipes" class="text-sm font-semibold leading-6 text-gray-900">Recipes</a>
+        <?php endif; ?>
+        <a href="/about" class="text-sm font-semibold leading-6 text-gray-900">About Us</a>
+        <a href="/contact" class="text-sm font-semibold leading-6 text-gray-900">Contact Us</a>
+        <?php if(isset($_SESSION['user']['email'])) : ?>    
+          <a href="/membership" class="text-sm font-semibold leading-6 text-gray-900">Membership</a>
+        <?php endif; ?>
       </div>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
 
@@ -38,7 +46,7 @@
     </nav>
     
     <!-- Mobile menu, show/hide based on menu open state. -->
-    <div class="lg:hidden" role="dialog" aria-modal="true">
+    <div class="lg:hidden" id="myModal" role="dialog" aria-modal="true">
       <!-- Background backdrop, show/hide based on slide-over state. -->
       <div class="fixed inset-0 z-50"></div>
       <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -49,25 +57,43 @@
             <span class="sr-only">My coffee shop</span>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
           </a>
-          <!-- Close button -->
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+          <!-- Close button - X -->
+          <button type="button" id="closeNavModal" class="-m-2.5 rounded-md p-2.5 text-gray-700">
             <span class="sr-only">Close menu</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
+
         <!-- Menu options for mobile -->
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
               <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
-              <a href="/recipes" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Recipes</a>
+              <?php if(isset($_SESSION['user']['email'])) : ?>    
+                <a href="/recipes" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Recipes</a>
+              <?php endif; ?>
               <a href="/about" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About us</a>
+              <a href="/contact" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact Us</a>
+              <?php if(isset($_SESSION['user']['email'])) : ?>    
+                <a href="/membership" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Membership</a>
+              <?php endif; ?>
             </div>
-            <div class="py-6">
-              <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
-            </div>
+            <?php if(! isset($_SESSION['user']['email'])) : ?>    
+              <div class="py-6">
+                <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+              </div>
+            <?php endif; ?>
+
+            <?php if(isset($_SESSION['user']['email'])) : ?>        
+
+              <form action="/logout" method="POST">
+                <button clas="text-sm font-semibold leading-6 text-gray-900">Log out</button>
+              </form>
+
+            <?php endif; ?>
+
           </div>
         </div>
       </div>
