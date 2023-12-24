fbdf77839b03e9f8c69097578dc667fc3ea2b597
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-24 12:50:05 -0400

    chore: update(head-section): modify head section, add script tag to import reCAPTCHA link from Google

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/styles.css">
+  <link rel="stylesheet" href="/css/styles.css">
+  <script src="/js/main.js"></script>
+  <script src="https://www.google.com/recaptcha/api.js?render=6LcbNzkpAAAAAPvT5x0b_m25lwwG9tKZfXqt5lbE"></script>
   <title>A Coffee Place</title>
 </head>
 <body class="h-full">
