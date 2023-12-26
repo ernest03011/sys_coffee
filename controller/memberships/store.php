1be6195ff4f24eef2c6e2711982958041987d05b
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-26 01:05:17 -0400

    chore: refactor(memberships): update structure in membership system

--- controller/memberships/store.php
+++ controller/memberships/store.php
@@ -7,23 +7,9 @@ if (!class_exists('Database')) {
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-// $current_id = 3;
-
-// $user_email = $_SESSION['user']['email'];
-
-// $user = $db->query("select * from users where email = :email", [
-//   'email' => $user_email
-// ])->find();
-
 // Testing JWT
 
 $user_id = getCurrentUserId();
-
-
-$start_date = new DateTime('now', new DateTimeZone('Asia/Taipei'));
-$expiration_date = clone $start_date;
-
-$expiration_date = $expiration_date->modify('+1 year')->format('Y-m-d');
 
 $billing_cycle = $_POST['billing_cycle'];
 
@@ -53,18 +39,17 @@ if(! $result){
   
   header('Location: ' . $redirectUrl);
   exit();
-  // require view('partials/membership.buy.php', $msg = [
-  //   'error' => 'Unable to create the subscription, double-check the details and try again!'
-  // ]); 
-
-  // exit();
 }
 
-// dd($result);
 
-require view('memberships/show.view.php', $user = [
+$data = [
   'status' => 1,
-  'billing_cycle' => $billing_cycle,
+  'subscription_duration' => $billing_cycle,
   'start_date' => $start_date->format('Y-m-d'),
   'expiration_date' => $expiration_date
+];
+
+require view('memberships/show.view.php', 
+[
+  'data' => $data
 ]);
