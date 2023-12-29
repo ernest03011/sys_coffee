cb29a1de75631be7ab6993acd32f1a5a1bc2ad35
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-28 23:16:59 -0400

    chore: add(smtp-settings): incorporate SMTP settings into config.php

--- config.php
+++ config.php
@@ -8,6 +8,18 @@
       'dbname' => 'coffee_db',
       'charset' => 'utf8mb4',
       'password' => 'anaphoric-panchions-sdeign'
-    ]
-  ]
-?>
+    ], 
+    'smtp' => [
+      'debug' => 0,
+      'host' => 'smtp.gmail.com',
+      'port' => 465,
+      'secure' => 'ssl',
+      'auth' => true,
+      'username' => 'manutechproject2023@gmail.com',
+      'password' => 'kyobhdemvglooxqw',
+      'set_from' => 'manutechproject2023@gmail.com',
+      'add_reply_to' => 'manutechproject2023@gmail.com',
+      'receiver_address' => 'manutechproject2023@gmail.com'
+    ],
+  ];
+  
