2fe8e1dde5cd6fde44378ddf877d77a47cbdf312
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-28 23:32:39 -0400

    chore: add(redirect-function): refactor code to utilize a redirect function

--- controller/rating/create.php
+++ controller/rating/create.php
@@ -7,25 +7,44 @@ $user_id = getCurrentUserId();
 $rating_value = $_POST['rating_value'];
 $recipe_id = $_POST['id'];
 
+// if(!Validator::string($rating)){
+//   $errors['username'] = 'The username is required';
+// }
+// if(!Validator::string($password, 8, 255)){
+//   $errors['password'] = 'The password is required';
+// }
+
+
+// dd($recipe_id);
+
 $rating = RecipeRating::saveToDatabase($attributes = [
   'recipe_id' => $recipe_id,
   'user_id' => $user_id, 
   'rating' => $rating_value
 ]);
 
-$redirectUrl = $_SERVER["HTTP_REFERER"];
+// dd($rating);
+$url = $_SERVER["HTTP_REFERER"];
 if(! $rating){
 
-  $errorMsg = 'Unable to insert the recipe';
-  $redirectUrl = $redirectUrl . '&message=' . urlencode($errorMsg) . '&type=error&color=red';
+  $errorMsg = 'Unable to reate this recipe';
+  // $redirectUrl = $redirectUrl . '&message=' . urlencode($errorMsg) . '&type=error&color=red';
   
-  header('Location: ' . $redirectUrl);
-  exit();
+  // header('Location: ' . $redirectUrl);
+  // exit();
+  redirect($url, [
+    'message' => urlencode($errorMsg),
+    'modifiers' => 'type=error&color=red'
+  ]);
 }
 
 $errorMsg = 'Great, you have rated this recipe!';
-$redirectUrl = $redirectUrl . '&message=' . urlencode($errorMsg) . '&type=error&color=green';
+// $redirectUrl = $redirectUrl . '&message=' . urlencode($errorMsg) . '&type=error&color=green';
 
-header('Location: ' . $redirectUrl);
-exit();
+// header('Location: ' . $redirectUrl);
+// exit();
 
+redirect($url, [
+  'message' => urlencode($errorMsg),
+  'modifiers' => 'type=error&color=green'
+]);
