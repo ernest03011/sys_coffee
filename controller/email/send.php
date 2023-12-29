6ff6eecdcd9fbb210342e3379b858d8144362cb4
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-12-28 23:15:20 -0400

    chore: refactor(email/send): enhance validation for specific inputs, extract SMTP configuration to config.php, and require the config file in email/send.php

--- controller/email/send.php
+++ controller/email/send.php
@@ -4,6 +4,12 @@ require base_path('vendor/autoload.php');
 use PHPMailer\PHPMailer\PHPMailer;
 $mail = new PHPMailer;
 
+// Load SMTP settings
+$config = require base_path('config.php');
+extract($config['smtp'], EXTR_PREFIX_ALL, 'smtp');
+
+require base_path('Validator.php');
+
 $secret_key = '6LcbNzkpAAAAAE7_vzhWXaHONMeu89J4mJewKcmx';
 
 $post_data = $val_err = $status_msg = '';
@@ -15,6 +21,17 @@ if(isset($_POST['submit_frm'])){
   $name = trim($_POST['name']);
   $email = trim($_POST['email']);
   $message = trim($_POST['message']);
+
+  if(!Validator::string($name)){
+    $val_err .= 'A name is required. <br/>';
+  }
+  if(!Validator::email($email)){
+    $val_err .= 'The email is required. <br/>';
+  }
+  if(!Validator::string($message)){
+    $val_err .= 'The message is required. <br/>';
+  }
+
 
   if(empty($name)){
     $val_err .= 'Please enter your name. <br/>';
@@ -65,16 +82,16 @@ if(isset($_POST['submit_frm'])){
       if($response_data["success"]){
 
         $mail->isSMTP();
-        $mail->SMTPDebug = 0;
-        $mail->Host = 'smtp.gmail.com';
-        $mail->Port = 465;
-        $mail->SMTPSecure = 'ssl';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'manutechproject2023@gmail.com';
-        $mail->Password = 'kyobhdemvglooxqw';
-        $mail->setFrom('manutechproject2023@gmail.com', 'Your Name');
-        $mail->addReplyTo('manutechproject2023@gmail.com', 'Your Name');
-        $mail->addAddress('manutechproject2023@gmail.com', 'Receiver Name');
+        $mail->SMTPDebug = $smtp_debug;
+        $mail->Host = $smtp_host;
+        $mail->Port = $smtp_port;
+        $mail->SMTPSecure = $smtp_secure;
+        $mail->SMTPAuth = $smtp_auth;
+        $mail->Username = $smtp_username;
+        $mail->Password = $smtp_password;
+        $mail->setFrom("{$smtp_set_from}", 'Your Name');
+        $mail->addReplyTo("{$smtp_add_reply_to}", 'Your Name');
+        $mail->addAddress("{$smtp_receiver_address}", 'Receiver Name');
         $mail->Subject = 'Checking if PHPMailer works';
         // $mail->msgHTML(file_get_contents('message.html'), __DIR__ . '/../' . "controller/email/" );
         $mail->Body = "This is just a plain text message body {$name}, {$email}, {$message}";
