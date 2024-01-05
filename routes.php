3d1b970e6cf92a02591713dea59b30336d1f9a93
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-01-05 13:01:09 -0400

    chore: remove(controller-from-routes): eliminate controller from all routes, update router require line to use namespace

--- routes.php
+++ routes.php
@@ -1,34 +1,36 @@
 <?php
 
+use Core\Router;
+
 $router = new Router();
 
-$router->get('/', '/controller/index.php');
-$router->get('/about', '/controller/about.php');
-$router->get('/test', '/controller/test.php');
+$router->get('/', '/index.php');
+$router->get('/about', '/about.php');
+$router->get('/test', '/test.php');
 
-$router->get('/contact', '/controller/contact.php');
-$router->post('/contact', '/controller/email/send.php');
-
-
-$router->get('/login', '/controller/session/create.php')->only('guest');
-$router->post('/login', '/controller/session/store.php')->only('guest');
-$router->post('/logout', '/controller/session/destroy.php')->only('auth');
-
-$router->get('/register', '/controller/registration/create.php')->only('guest');
-$router->post('/register', '/controller/registration/store.php')->only('guest');
-
-$router->get('/submit-recipe', '/controller/recipes/create.php')->only('auth');
-$router->post('/submit-recipe', '/controller/recipes/store.php')->only('auth');
-
-$router->get('/recipes', '/controller/recipes/index.php')->only('auth');
-$router->get('/recipe', '/controller/recipes/show.php')->only('auth');
-$router->delete('/recipe', '/controller/recipes/destroy.php')->only('auth');
+$router->get('/contact', '/contact.php');
+$router->post('/contact', '/email/send.php');
 
 
-$router->get('/recipe/edit/', '/controller/recipes/edit.php')->only('auth');
-$router->patch('/recipe', '/controller/recipes/update.php')->only('auth');
+$router->get('/login', '/session/create.php')->only('guest');
+$router->post('/login', '/session/store.php')->only('guest');
+$router->post('/logout', '/session/destroy.php')->only('auth');
 
-$router->post('/rating', '/controller/rating/create.php')->only('auth');
+$router->get('/register', '/registration/create.php')->only('guest');
+$router->post('/register', '/registration/store.php')->only('guest');
 
-$router->get('/membership', '/controller/memberships/index.php')->only('auth');
-$router->post('/membership', '/controller/memberships/store.php')->only('auth');
+$router->get('/submit-recipe', '/recipes/create.php')->only('auth');
+$router->post('/submit-recipe', '/recipes/store.php')->only('auth');
+
+$router->get('/recipes', '/recipes/index.php')->only('auth');
+$router->get('/recipe', '/recipes/show.php')->only('auth');
+$router->delete('/recipe', '/recipes/destroy.php')->only('auth');
+
+
+$router->get('/recipe/edit/', '/recipes/edit.php')->only('auth');
+$router->patch('/recipe', '/recipes/update.php')->only('auth');
+
+$router->post('/rating', '/rating/create.php')->only('auth');
+
+$router->get('/membership', '/memberships/index.php')->only('auth');
+$router->post('/membership', '/memberships/store.php')->only('auth');
