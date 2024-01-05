bb6a2bf95c3c2d42255f6f97f70ec8bcdcc8f930
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-01-05 12:57:59 -0400

    chore: add(require-vendor-autoload): include 'require vendor autoload' statement, update functions and router require line to search under /Core/ directory

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@ const BASE_PATH = __DIR__ . '/../';
 
 session_start();
 
-require BASE_PATH . 'functions.php';
-$router = require base_path('Router.php');
+require BASE_PATH . 'Core/functions.php';
+require_once BASE_PATH . 'vendor/autoload.php';
+$router = require base_path('Core/Router.php');
 require base_path('routes.php');
 
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
