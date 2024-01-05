f21874bb34491773fab7251daee583cb0da00003
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-01-05 13:11:43 -0400

    chore: add(recipes-section-link): include text with a link to redirect users to the section for submitting recipes

--- views/recipes/index.view.php
+++ views/recipes/index.view.php
@@ -5,6 +5,10 @@
 <?php require view('partials/banner.php'); ?>
 
 
+<div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 bg-blue-700">
+  <a href="/submit-recipe" class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-blue-500 shadow-sm hover:text-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700">Add Recipe</a>
+</div>
+
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center">
 
   <?php
