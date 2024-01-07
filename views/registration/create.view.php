1d6ce387630fa42a7328656886105c082ad16293
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-01-06 22:59:25 -0400

    chore: add(confirm-password): include a confirmation password button, enhance styling for the registration page, and add block code to display registration error messages

--- views/registration/create.view.php
+++ views/registration/create.view.php
@@ -10,11 +10,17 @@
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create a new user</h2>
     </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="flex flex-col mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="/register" method="POST">
+
+        <!-- Registration Failed Message -->
+        <?php if(isset($errors['registration'])) : ?>
+          <p class="text-red-500"> <?= htmlspecialchars($errors['registration']) ?>  </p>
+        <?php endif; ?>
+
         <!-- Username -->
         <?php if(isset($errors['username'])) : ?>
-          <p class="text-red-500"> <?= $errors['username'] ?>  </p>
+          <p class="text-red-500"> <?= htmlspecialchars($errors['username']) ?>  </p>
         <?php endif; ?>
 
         <div>
@@ -26,7 +32,7 @@
         
         <!-- Email -->
         <?php if(isset($errors['email'])) : ?>
-          <p class="text-red-500"> <?= $errors['email'] ?>  </p>
+          <p class="text-red-500"> <?= htmlspecialchars($errors['email']) ?>  </p>
         <?php endif; ?>
 
         <div>
@@ -38,27 +44,34 @@
 
         <!-- Password -->
         <?php if(isset($errors['password'])) : ?>
-          <p class="text-red-500"> <?= $errors['password'] ?>  </p>
+          <p class="text-red-500"> <?= htmlspecialchars($errors['password']) ?>  </p>
         <?php endif; ?>
 
-        <div class="flex items-center justify-between">
+        <div class="mt-2">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
             <div class="mt-2">
               <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-            </div>
+        </div>
+        <div class="mt-2">
+          <label for="conPassword" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+          <div class="mt-2">
+            <input id="conPassword" name="conPassword" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
         </div>
 
-        <div>
+        <div class="mt-5">
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
 
       </form>
-      <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+
+      <div class="flex flex-col mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
         <p class="mt-10 text-center text-sm text-red-500">
           Have an account?
           <a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log in</a>
         </p>
       </div>
+      
     </div>
   </div>
 
