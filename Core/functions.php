ddc1230abb6fdb072b1c29cd559ba46cad9a6254
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-02-02 23:16:56 -0400

    chore: remove(getcurrentuserid): eliminate the getcurrentuserid function as it has been integrated into the session/manager.php file

--- Core/functions.php
+++ Core/functions.php
@@ -32,42 +32,6 @@ function abort($code = 404)
     die();
 }
 
-function getCurrentUserId()
-{
-
-    $has_token = isset($_SESSION['user']['jwt_token']) ? true : false;
-    $data = '';
-    $jwt = new JwtHandler();
-
-    $config = require base_path('config.php');
-    $db = new Database($config['database']);
-
-    if ($has_token) {
-        $token = $_SESSION['user']['jwt_token'];
-
-        $data = $jwt->decode($token);
-
-    }
-
-    if (! $data == '') {
-
-        $user = $db->query('select * from users where email = :email', [
-            'email' => $_SESSION['user']['email'],
-        ])->find();
-
-        //Payload can be anything you want to store in the token
-        $payload = $user['user_id'];
-
-        $jwtToken = $jwt->encode("http://localhost:8080/", $payload);
-
-        $_SESSION['user']['jwt_token'] = (string) $jwtToken;
-
-        $data = $payload;
-    }
-
-    return $data;
-}
-
 function redirect($url, $attributes = [])
 {
     // Use parse_url to get the components of the URL
