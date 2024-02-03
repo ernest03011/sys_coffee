d148666bea312f45e017e236afe74232e8b9d7bb
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-02-02 23:30:46 -0400

    chore: refactor(code): update code to utilize the new getcurrentuserid and hasanactivemembership classes

--- Http/controller/memberships/index.php
+++ Http/controller/memberships/index.php
@@ -1,12 +1,15 @@
 <?php
 
 use Core\Database;
+use Http\controller\session\Manager;
+use Http\controller\memberships\MembershipManager;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
 // working with JWT
-$user_id = getCurrentUserId();
+$user_id = Manager::getCurrentUserId();
+// $user_id = 4; // getCurrentUserId();
 
 if(! isset($user_id)){
 
@@ -17,13 +20,16 @@ if(! isset($user_id)){
 
   exit();
 }
+// does the user have a memebership?
+// $result = $db->query('select * from memberships where user_id = :user_id', [
+//   'user_id' => $user_id 
+// ])->get();
 
-$result = $db->query('select * from memberships where user_id = :user_id', [
-  'user_id' => $user_id 
-])->get();
+$membershipManager = new MembershipManager($db);
+$result = $membershipManager->hasAnActiveMembership($user_id);
 
-
-if(count($result) == 0){
+// if(count($result) == 0){
+if(! $result){
 
   require view('memberships/create.view.php', 
   [
