4aa53c1b3fe1d2cc33e6345421028b95a8f88fb8
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-02-03 22:54:13 -0400

    chore: refactor(membership-layout): optimize layout for displaying membership information

--- views/memberships/show.view.php
+++ views/memberships/show.view.php
@@ -4,13 +4,26 @@
 
 <?php require view('partials/banner.php'); ?>
 
-<div>
 
-  <p>Status: <?= $data['active'] == 1 ? 'active' : 'disabled'?></p>
-  <p>Billing Cycle: <?= $data['subscription_duration'] ?> month(s)</p>
-  <p>Start Date: <?= $data['start_date'] ?></p>
-  <p>Expiration Date: <?= $data['expiration_date'] ?></p>
-  
+<div class="max-w-md flex items-center justify-center bg-white rounded-xl overflow-hidden border-l-4 border-orange-900 shadow-md">
+    <div class="p-4">
+        <div class="flex items-center justify-center">
+            <svg class="h-8 w-8 text-coffee-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 18v-6a9 9 0 0118 0v6"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21v-6a9 9 0 00-9-9"></path>
+                <path d="M3 18v-6a9 9 0 0118 0v6"></path>
+                <path d="M21 21v-6a9 9 0 00-9-9"></path>
+            </svg>
+        </div>
+        <div class="mt-4 text-center">
+            <h1 class="text-xl font-semibold">Membership Subscription</h1>
+            <p>Status: <?= $data['active'] == 1 ? 'active' : 'disabled'?></p>
+            <p>Billing Cycle: <?= $data['subscription_duration'] ?> month(s)</p>
+            <p>Start Date: <?= $data['start_date'] ?></p>
+            <p>Expiration Date: <?= $data['expiration_date'] ?></p>
+        </div>
+    </div>
 </div>
+
 
 <?php require view('partials/footer.php'); ?>
