ea3745ac55ce0265bfaa872da833df8038d2e305
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-02-03 22:58:19 -0400

    chore: update(color-scheme): modify color scheme in the project.

--- views/partials/section.rating.php
+++ views/partials/section.rating.php
@@ -1,19 +1,19 @@
 <h3>Rate this recipe!</h3>
 <br/>
 <ul class="grid grid-flow-col">
-  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="submitRatingForm(1)">
+  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-950 focus:outline-none focus:ring-2 focus:ring-yellow-950 focus:ring-offset-2" onclick="submitRatingForm(1)">
     1
   </li>
-  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="submitRatingForm(2)">
+  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-950 focus:outline-none focus:ring-2 focus:ring-yellow-950 focus:ring-offset-2" onclick="submitRatingForm(2)">
     2
   </li>
-  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="submitRatingForm(3)">
+  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-950 focus:outline-none focus:ring-2 focus:ring-yellow-950 focus:ring-offset-2" onclick="submitRatingForm(3)">
     3
   </li>
-  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="submitRatingForm(4)">
+  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-950 focus:outline-none focus:ring-2 focus:ring-yellow-950 focus:ring-offset-2" onclick="submitRatingForm(4)">
     4
   </li>
-  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onclick="submitRatingForm(5)">
+  <li class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-950 focus:outline-none focus:ring-2 focus:ring-yellow-950 focus:ring-offset-2" onclick="submitRatingForm(5)">
     5
   </li>
 </ul>
