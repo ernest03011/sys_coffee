b28a91c5b9efd144305b3e7c4423d9f338943ad6
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-02-05 23:41:44 -0400

    chore: update(tailwind-css-logo-color): modify the color for the Tailwind CSS logo in the nav menu

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,7 @@
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">My coffee shop</span>
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=yellow&shade=950" alt="">
         </a>
       </div>
       <!-- Hamburguer icon - OPEN BUTTON -->
